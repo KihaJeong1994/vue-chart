0bbfead30d9c8cf08f9d496974265a4df1b21acb
KihaJeong1994 <jkha7371@gmail.com>
2022-02-07 16:54:58 +0900

    vue chart plugin

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,31 @@
+<template>
+    <canvas id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+    props : ['propsdata'],
+mounted() {
+    const ctx = document.getElementById('barChart');
+    const myChart = new this.$_Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    console.log(myChart)
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,43 @@
+<template>
+    <canvas id="lineChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+    mounted() {
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+        const myChart = new this.$_Chart(
+            document.getElementById('lineChart'),
+            config
+        );
+        console.log(myChart)
+    },
+}
+</script>
+
+<style>
+
+</style>
